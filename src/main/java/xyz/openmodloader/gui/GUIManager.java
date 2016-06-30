7ecdf8e31eccc8a836aaa2af3d345e9814ae919c
Shadowfacts <me@shadowfacts.net>
2016-06-30 14:54:48 -0400

    Replace modID with owner

--- src/main/java/xyz/openmodloader/gui/GUIManager.java
+++ src/main/java/xyz/openmodloader/gui/GUIManager.java
@@ -10,28 +10,28 @@ public class GUIManager {
 
     private static final Map<String, GUIHandler> handlers = new HashMap<>();
 
-    public static void register(String id, GUIHandler handler) {
-        handlers.put(id, handler);
+    public static void register(String owner, GUIHandler handler) {
+        handlers.put(owner, handler);
     }
 
-    public static GUIHandler get(String id) {
-        return handlers.get(id);
+    public static GUIHandler get(String owner) {
+        return handlers.get(owner);
     }
 
-    public static void open(EntityPlayer player, String modID, String guiID, Context context) {
+    public static void open(EntityPlayer player, String owner, String guiID, Context context) {
         if (player.worldObj.isRemote) {
-            openClient(player, modID, guiID, context);
+            openClient(player, owner, guiID, context);
         } else {
-            openServer(player, modID, guiID, context);
+            openServer(player, owner, guiID, context);
         }
     }
 
-    private static void openClient(EntityPlayer player, String modID, String guiID, Context context) {
-        Minecraft.getMinecraft().displayGuiScreen(handlers.get(modID).getGUI(guiID, context));
+    private static void openClient(EntityPlayer player, String owner, String guiID, Context context) {
+        Minecraft.getMinecraft().displayGuiScreen(handlers.get(owner).getGUI(guiID, context));
     }
 
-    private static void openServer(EntityPlayer player, String modID, String guiID, Context context) {
-        player.openContainer = handlers.get(modID).getContainer(guiID, context);
+    private static void openServer(EntityPlayer player, String modID, String owner, Context context) {
+        player.openContainer = handlers.get(modID).getContainer(owner, context);
     }
 
 }
