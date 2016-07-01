af21bdb2cbcb01f6d8be4f6003c835b0ffab2a5c
CoolSquid <mail@coolsquid.me>
2016-07-01 17:48:44 +0200

    Add helper methods to Side and Environment

--- src/main/java/xyz/openmodloader/launcher/strippable/Environment.java
+++ src/main/java/xyz/openmodloader/launcher/strippable/Environment.java
@@ -1,7 +1,22 @@
 package xyz.openmodloader.launcher.strippable;
 
+import xyz.openmodloader.launcher.OMLStrippableTransformer;
+
 public enum Environment {
+
     PRODUCTION,
     DEVELOPMENT,
-    UNIVERSAL
+    UNIVERSAL;
+
+    public static boolean isDevelopment() {
+        return getCurrent() == DEVELOPMENT;
+    }
+
+    public static boolean isProduction() {
+        return getCurrent() == PRODUCTION;
+    }
+
+    public static Environment getCurrent() {
+        return OMLStrippableTransformer.getEnvironment();
+    }
 }
--- src/main/java/xyz/openmodloader/launcher/strippable/Side.java
+++ src/main/java/xyz/openmodloader/launcher/strippable/Side.java
@@ -1,7 +1,22 @@
 package xyz.openmodloader.launcher.strippable;
 
+import xyz.openmodloader.launcher.OMLStrippableTransformer;
+
 public enum Side {
+
     CLIENT,
     SERVER,
-    UNIVERSAL
+    UNIVERSAL;
+
+    public static boolean isClient() {
+        return getCurrent() == CLIENT;
+    }
+
+    public static boolean isServer() {
+        return getCurrent() == SERVER;
+    }
+
+    public static Side getCurrent() {
+        return OMLStrippableTransformer.getSide();
+    }
 }
